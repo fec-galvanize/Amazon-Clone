12e858ab298ab8fe29a1dfa06eb7b9f12974a6f9
Moses V <mov7963@gmail.com>
2022-08-30 18:46:03 -0400

    Integrated Redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import "../styles/globals.css";
 import Meta from "../components/Meta";
 import Header from "../components/Header";
+import { Provider } from "react-redux";
+import { store } from "../redux";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <Meta />
       <Header />
       <Component {...pageProps} />
-
-    </>
+    </Provider>
   );
 }
 
