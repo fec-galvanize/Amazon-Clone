98e9a1b5dbfc8a5e2b15752ab3f9cbec1a4b1ff3
Ilsa <hamptonilsa96@gmail.com>
2022-08-30 15:30:41 -0400

    I added the header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import Meta from "../components/Meta";
 import Middle from "../components/Middle";
+import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Meta />
       <Component {...pageProps} />
+      <Header />
       <Middle />
     </>
   );
