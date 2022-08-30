6692e3f269c7133f37fe7c2080b1aff827d3414b
Moses V <mov7963@gmail.com>
2022-08-30 15:47:29 -0400

    Set up Product Image Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import "../styles/globals.css";
 import Meta from "../components/Meta";
-import Middle from "../components/Middle";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Meta />
       <Component {...pageProps} />
-      <Middle />
+      
     </>
   );
 }
