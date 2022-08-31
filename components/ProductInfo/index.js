d989659e93c101e616673902dbc3371a83a5598e
Moses <mov7963@gmail.com>
2022-08-31 16:08:39 -0400

    Merged with main
    -Restructured file path for organization
    - Deleted un-used images from the image folder
    - Fixed import statements

--- components/ProductInfo/index.js
+++ components/ProductInfo/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Cart from './Cart'
 import ProdImage from './ProdImage'
+import Middle from './middle/Middle'
 
 import style from '../../styles/ProdInfo.module.css'
 const ProductInfo = () => {
   return (
     <div className={`${style.container}`}>
         <ProdImage />
-
+        <Middle />
         <Cart />
     </div>
   )
