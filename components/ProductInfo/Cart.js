c9a0e6bae254eee66d1e662ef319c03129174d96
Edward Sabbatino <104376785+EntropicWind@users.noreply.github.com>
2022-09-01 16:00:04 -0700

    cart complete minus hover dialogue boxes

--- components/ProductInfo/Cart.js
+++ components/ProductInfo/Cart.js
@@ -1,129 +1,3 @@
-// import style from "../../styles/Cart.module.css";
-
-// const Cart = () => {
-//   const addToCart = () => {};
-
-//   const qtyDropdown = () => {};
-
-//   const buyNow = () => {};
-//   const addToList = () => {};
-
-//   return (
-//     <div className={`${style.container}`}>
-//       <div>
-//         <div className="pricing">
-//           <div className="numbers">
-//             <span id="symbol">$</span>
-//             <span id="dollars">17</span>
-//             <span id="decimal">.</span>
-//             <span id="fraction">99</span>
-//           </div>
-//         </div>
-//         <br></br>
-//         <div>
-//           <span id="shipping-message">
-//             Get
-//             <b> Fast, Free Shipping </b>
-//             with <br></br>
-//             <a
-//               href="https://www.amazon.com/amazonprime?_encoding=UTF8&primeCampaignId=primedp_ventures_desktopBelowThreshold&ref=primedp_ventures_desktopBelowThreshold"
-//               target="_blank"
-//             >
-//               {" "}
-//               Amazon Prime{" "}
-//             </a>
-//           </span>
-//           <br></br>
-//           <span>
-//             <a className="popover-trigger" href="javascript:void(0)">
-//               FREE Returns
-//             </a>
-//           </span>
-//         </div>
-//         <br></br>
-//         <div className="delivery-block">
-//           <a
-//             className="delivery-link"
-//             href="https://www.amazon.com/gp/help/customer/display.html?nodeId=GZXW7X6AKTHNUP6H"
-//           >
-//             {" "}
-//             FREE delivery{" "}
-//           </a>
-//           <span className="text-bold">
-//             <span>
-//               <b>Tuesday, </b>
-//             </span>{" "}
-//             <br></br>
-//             <span>
-//               <b>September 6 </b>
-//             </span>
-//           </span>
-//           if you spend $25 <br></br> on items shipped by Amazon
-//         </div>
-//         <br></br>
-//         <div className="fastest-delivery-block">
-//           <span className="text-bold">
-//             <span>
-//               Or fastest delivary <b>Saturday,</b>
-//               <br></br>
-//               <b>September 3. </b> Order within{" "}
-//             </span>{" "}
-//             <br></br>
-//             <span id="countdown">
-//               <span className="hours" style={{ color: "green" }}>
-//                 13 hrs{" "}
-//               </span>
-//               <span className="minutes" style={{ color: "green" }}>
-//                 21 mins
-//               </span>
-//             </span>
-//           </span>
-//         </div>
-//         <br></br>
-//         <div className="select-location">
-//           <a href="javascript:void(0)"> Select delivary location </a>
-//         </div>
-
-//         <div>
-//           <p style={{ color: "green" }}> In Stock. </p>
-//           <button className="btn-quantity" onClick={qtyDropdown}>
-//             Qty:
-//           </button>
-//         </div>
-//         <br></br>
-//         <div>
-//           <button
-//             className={`${style.btnCart}`}
-//             style={{ backgroundColor: "yellow", color: "black" }}
-//             onClick={addToCart}
-//           >
-//             Add to Cart
-//           </button>
-//         </div>
-//         <div>
-//           <button
-//             className={`${style.btnBuyNow}`}
-//             style={{ backgroundColor: "orange", color: "black" }}
-//             onClick={buyNow}
-//           >
-//             {" "}
-//             Buy Now
-//           </button>
-//         </div>
-//         <br></br>
-//         <div id="secure-transaction"></div>
-//         <div>
-//           <button className={`${style.btnAdd}`} onClick={addToList}>
-//             Add to List
-//           </button>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Cart;
-
 import React from "react";
 import style from "../../styles/Cart.module.css";
 
@@ -159,8 +33,10 @@ const addToList = () => {
                 </span>
                 <br></br>
                 <span>
-                    <a className="popover-trigger" href="javascript:void(0)">
+                    <a className="popover-trigger" href="#">
                     FREE Returns 
+                    <div id={style.icon}></div> 
+                    <div className={style.arrowBox} aria-modal="true"></div>
                     </a>
                 </span>
             </div>
@@ -184,14 +60,14 @@ const addToList = () => {
             </div><br></br>
             <div className="select-location">
                 <div id={`${style.pinPoint}`}></div>
-                <a href="javascript:void(0)" id={style.location}> Select delivary location </a>
+                <a href="#" id={style.location}> Select delivary location </a>
             </div>
         
         <div>
             <p className={style.stock}> In Stock. </p>
             <span className={style.boxshadow}>
              <span className={`${style.quantity}`}>
-             <label for="select-quantity"> Qty: </label>
+             <label htmlFor="select-quantity"> Qty: </label>
              <select className={`${style.selectQuantity}`} name="select-quantity">
                <option value="1">1</option>
                <option value="2">2</option>
@@ -210,18 +86,18 @@ const addToList = () => {
 
              <div>
               <span>
-                 <button className={`${style.btnCart}`} title="Add to Shopping Cart" onClick={addToCart}>Add to Cart</button> 
+                 <button className="button" id={`${style.btnCart}`} title="Add to Shopping Cart" onClick={addToCart}>Add to Cart</button> 
               </span>
              </div>
 
              <div>
-                 <button className={`${style.btnBuyNow}`} onClick={buyNow}> Buy Now</button> 
+                 <button className="button" id={`${style.btnBuyNow}`} onClick={buyNow}> Buy Now</button> 
             </div><br></br>
             
             <div id={style.secure}>
              <div> 
                 <img src="https://images-na.ssl-images-amazon.com/images/G/01/x-locale/checkout/truespc/secured-ssl._CB485936932_.png" height="15px"></img>
-                <a href="javascript:void(0)"> Secure transaction</a>
+                <a href="#"> Secure transaction</a>
             </div>
             </div><br></br>
 
@@ -234,8 +110,11 @@ const addToList = () => {
 
             <div className={style.returnpolicy}>
               <span>Return policy: </span>
-              <a href="javascript:void(0)" role="button">Eligible for Return, 
-              Refund or Replacement within 30 days of receipt</a><span id={style.icon}></span> 
+              <a href="#" role="button">Eligible for Return, 
+              Refund or Replacement within 30 days of receipt
+              <div id={style.icon}></div> 
+              </a>
+              
             </div><br></br>
 
             <div className={style.primerow}>
