062c7b79f4d5fd533fee3b0c0f11423abf41fd6e
Edward Sabbatino <104376785+EntropicWind@users.noreply.github.com>
2022-09-01 12:49:17 -0700

    near complete cart

--- components/cart.js
+++ components/cart.js
@@ -25,7 +25,6 @@ const addToList = () => {
                 <div className="numbers">
                     <span id="symbol">$</span>
                     <span id="dollars">17</span>
-                    <span id="decimal">.</span>
                     <span id={`${style.fraction}`}>99</span>
                 </div>
             </div>
@@ -35,48 +34,48 @@ const addToList = () => {
                 Get
                 <b> Fast, Free Shipping </b>
                 with <br></br>
-                <a href="https://www.amazon.com/amazonprime?_encoding=UTF8&primeCampaignId=primedp_ventures_desktopBelowThreshold&ref=primedp_ventures_desktopBelowThreshold" target="_blank"> Amazon Prime </a>
+                <a href="https://www.amazon.com/amazonprime?_encoding=UTF8&primeCampaignId=primedp_ventures_desktopBelowThreshold&ref=primedp_ventures_desktopBelowThreshold"> Amazon Prime </a>
                 </span>
                 <br></br>
                 <span>
-                    <a class="popover-trigger" href="javascript:void(0)">
+                    <a className="popover-trigger" href="javascript:void(0)">
                     FREE Returns 
                     </a>
                 </span>
             </div>
             <br></br>
             <div className="delivery-block">
-                <a class="delivery-link" href="https://www.amazon.com/gp/help/customer/display.html?nodeId=GZXW7X6AKTHNUP6H"> FREE delivery </a>
-                <span class="text-bold">
+                <a className="delivery-link" href="https://www.amazon.com/gp/help/customer/display.html?nodeId=GZXW7X6AKTHNUP6H"> FREE delivery </a>
+                <span className="text-bold">
                     <span><b>Tuesday, </b></span> <br></br>
                     <span><b>September 6 </b></span>
                 </span>
                 if you spend $25 <br></br> on items shipped by Amazon
             </div><br></br>
             <div className="fastest-delivery-block">
-                <span class="text-bold">
+                <span className="text-bold">
                     <span>Or fastest delivary <b>Saturday,</b><br></br><b>September 3. </b> Order within </span> <br></br>
                     <span id="countdown">
-                        <span class="hours" style={{color: 'rgb(0, 118, 0)'}}>13 hrs </span>
-                        <span class="minutes" style={{color: 'green'}}>21 mins</span>
+                        <span className="hours" style={{color: 'rgb(0, 118, 0)'}}>13 hrs </span>
+                        <span className="minutes" style={{color: 'green'}}>21 mins</span>
                     </span>
                 </span>
             </div><br></br>
-            <div class="select-location">
+            <div className="select-location">
                 <div id={`${style.pinPoint}`}></div>
                 <a href="javascript:void(0)"> Select delivary location </a>
             </div>
         
         <div>
             <p style={{color: 'green'}}> In Stock. </p>
-            <button className="btn-quantity" onClick={qtyDropdown}>Qty:</button>      
+            <select className="select-quantity" onClick={qtyDropdown}>Qty:</select>      
         </div><br></br>
 
              <div>
-                 <button class={`${style.btnCart}`} title="Add to Shopping Cart" style={{backgroundColor: 'FFD814', color: 'black', }}  onClick={addToCart}>Add to Cart</button> 
+                 <button className={`${style.btnCart}`} title="Add to Shopping Cart" style={{backgroundColor: 'FFD814', color: 'black', }}  onClick={addToCart}>Add to Cart</button> 
              </div>
              <div>
-                 <button class={`${style.btnBuyNow}`} style={{backgroundColor: 'orange', color: 'black'}} onClick={buyNow}> Buy Now</button> 
+                 <button className={`${style.btnBuyNow}`} style={{backgroundColor: 'orange', color: 'black'}} onClick={buyNow}> Buy Now</button> 
             </div><br></br>
             
             <div id="secure-transaction">
@@ -86,7 +85,8 @@ const addToList = () => {
             </div>
             </div><br></br>
             <div>
-
+                <span> Ships from         Amazon.com</span>
+                <span> Sold By         Amazon.poop</span>
             </div>
 
 
