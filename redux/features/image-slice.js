9b1d113644c6775de36875b48ee55066d0cb8998
Jacob <jacob.d.walters22@gmail.com>
2022-09-01 13:32:07 -0400

    refactored html to display images

--- redux/features/image-slice.js
+++ redux/features/image-slice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   image:
-    "https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C81t54H7ZpXL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX385_.png"
+    "https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C81t54H7ZpXL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX385_.png",
 };
 
 export const imageSlice = createSlice({
@@ -15,5 +15,5 @@ export const imageSlice = createSlice({
   },
 });
 
-export const {changeImage} = imageSlice.actions
-export default imageSlice.reducer
+export const { changeImage } = imageSlice.actions;
+export default imageSlice.reducer;
