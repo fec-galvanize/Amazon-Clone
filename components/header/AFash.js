10054dcee2828e3bbc731b261b3944c9b52be16d
Ilsa <hamptonilsa96@gmail.com>
2022-08-31 23:16:42 -0400

    added some styling for the top component in header

--- components/header/AFash.js
+++ components/header/AFash.js
@@ -1,4 +1,3 @@
-
 export default function AFash() {
   return <div>AFash</div>;
 }
--- components/header/Top.js
+++ components/header/Top.js
@@ -1,13 +1,19 @@
-
 import amazonlogo from "../../public/images/amazon-logo-top.jpg";
+import React from "react";
+
+import style from "../../styles/Header.module.css";
 
 export default function Top() {
   return (
-    <div className="search">
-      <img src={amazonlogo} alt="amazon logo" />
-      <form>
-        <input type="text" />
+    <div className={`${style.search}`}>
+      <img className={`${style.logo}`} src={amazonlogo} alt="amazon logo" />
+      <form className={`${style.form}`}>
+        <input className={`${style.input}`} type="text" />
+        <button className={`${style.sbtn}`} type="submit">
+          <i className={`${style.sbm}`}></i>
+        </button>
       </form>
     </div>
   );
 }
+//comment
