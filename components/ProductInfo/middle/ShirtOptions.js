6febf6f862c4ce14354036183bd095e8fbc4f83b
Jacob <jacob.d.walters22@gmail.com>
2022-09-01 13:32:07 -0400

    refactored html to display images

--- components/ProductInfo/middle/ShirtOptions.js
+++ components/ProductInfo/middle/ShirtOptions.js
@@ -1,6 +1,54 @@
+import { useState } from "react";
 import style from "../../../styles/Middle.module.css";
 
 const ShirtOptions = () => {
+  const [gender, setGender] = useState("male");
+  const [color, setColor] = useState("");
+  const [see, setSee] = useState(false);
+
+  const shirtColors = [
+    {
+      color: "Black",
+      img: "https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Navy",
+      img: "https://m.media-amazon.com/images/I/A1vJUKBjc2L._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Ashphalt",
+      img: "https://m.media-amazon.com/images/I/A1rcXo55giL._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Cranberry",
+      img: "https://m.media-amazon.com/images/I/B1fs3pzGnVS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Kelly Green",
+      img: "https://m.media-amazon.com/images/I/B1VMTBKtipS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Dark Heather",
+      img: "https://m.media-amazon.com/images/I/B1MuEgxHlwS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Heather Grey",
+      img: "https://m.media-amazon.com/images/I/C1ce8y0uOwS._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Heather Blue",
+      img: "https://m.media-amazon.com/images/I/B1Rv34VM9pS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Purple",
+      img: "https://m.media-amazon.com/images/I/B1el7IZypsS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+    {
+      color: "Royal Blue",
+      img: "https://m.media-amazon.com/images/I/B1EryObaEWS._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png",
+    },
+  ];
+
   return (
     <div className={style.options}>
       <form className={style.optionsForm}>
@@ -27,117 +75,21 @@ const ShirtOptions = () => {
             </span>
           </div>
           <ul className={style.colorOptions}>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Black"
-                    src="https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Navy"
-                    src="https://m.media-amazon.com/images/I/A1vJUKBjc2L._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Ashphalt"
-                    src="https://m.media-amazon.com/images/I/A1rcXo55giL._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Cranberry"
-                    src="https://m.media-amazon.com/images/I/B1fs3pzGnVS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Kelly Green"
-                    src="https://m.media-amazon.com/images/I/B1VMTBKtipS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Dark Heather"
-                    src="https://m.media-amazon.com/images/I/B1MuEgxHlwS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png
-                  "
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Heather Grey"
-                    src="https://m.media-amazon.com/images/I/C1ce8y0uOwS._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Heather Blue"
-                    src="https://m.media-amazon.com/images/I/B1Rv34VM9pS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Purple"
-                    src="https://m.media-amazon.com/images/I/B1el7IZypsS._CLa%7C500%2C468%7C819u1EpKTtL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
-            <li className={style.option}>
-              <div className={style.optionBox}>
-                <button className={style.optionBtn}>
-                  <img
-                    className={style.optionIMG}
-                    alt="Royal Blue"
-                    src="https://m.media-amazon.com/images/I/B1EryObaEWS._CLa%7C500%2C468%7C81t54H7ZpXL.png%7C0%2C0%2C500%2C468%2B0.0%2C0.0%2C500.0%2C468.0._SX38_SY50_CR,0,0,38,50_.png"
-                  ></img>
-                </button>
-              </div>
-            </li>
+            {shirtColors.map((shirt, i) => {
+              return (
+                <li className={style.option} key={i}>
+                  <div className={style.optionBox}>
+                    <button className={style.optionBtn}>
+                      <img
+                        className={style.optionIMG}
+                        alt={shirt.color}
+                        src={shirt.img}
+                      ></img>
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
           </ul>
         </div>
 
--- redux/features/prodInfo-slice.js
+++ redux/features/prodInfo-slice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   image:
     "https://m.media-amazon.com/images/I/A13usaonutL._CLa%7C2140%2C2000%7C81t54H7ZpXL.png%7C0%2C0%2C2140%2C2000%2B0.0%2C0.0%2C2140.0%2C2000.0_AC_UX385_.png",
-  pageUrl: "http://localhost:3000/#"
+  pageUrl: "http://localhost:3000/#",
 };
 
 export const prodInfoSlice = createSlice({
