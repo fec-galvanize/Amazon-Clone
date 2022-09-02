c57bce8638234b9f62796ff15869868edf5f909c
Ilsa <hamptonilsa96@gmail.com>
2022-09-02 16:39:43 -0400

    fixing

---
+++ components/Meta.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+const Meta = () => {
+  return (
+    <Head>
+      <title>Amazon Clone </title>
+      <meta name="description" content="Amazon Clone" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
+
+export default Meta;
