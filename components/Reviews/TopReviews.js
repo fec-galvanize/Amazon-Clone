3bb31c276c830572da608581e56592e3a49d7ef3
Moses <mov7963@gmail.com>
2022-09-02 16:25:34 -0400

    Getting started on review body

---
+++ components/Reviews/TopReviews.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "../../styles/RevTopRevs.module.css";
+
+const TopReviews = () => {
+  return (
+    <div className={style.container} >
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default TopReviews
--- components/Reviews/index.js
+++ components/Reviews/index.js
@@ -1,8 +1,10 @@
 import Stats from "./Stats"
+import TopReviews from "./TopReviews"
 const Reviews = () => {
   return (
     <div>
       <Stats />
+      <TopReviews /> 
     </div>
   )
 }
