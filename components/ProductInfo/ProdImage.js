809bc722cd4e4bc3d660ec9073b0940b536c19cd
Moses <mov7963@gmail.com>
2022-09-01 20:37:24 -0400

    Closes link display after clicking link

--- components/ProductInfo/ProdImage.js
+++ components/ProductInfo/ProdImage.js
@@ -41,9 +41,9 @@ export default ProdImage;
 
 const ImgBtn = ({ image }) => {
   const dispatch = useDispatch();
-  const {  imgArray, currentImage } = useSelector(({ prodInfo }) => ({
+  const { imgArray, currentImage } = useSelector(({ prodInfo }) => ({
     imgArray: prodInfo.imageBtnArr,
-    currentImage: prodInfo.currentImage
+    currentImage: prodInfo.currentImage,
   }));
 
   return (
@@ -79,22 +79,46 @@ const ShareBtn = () => {
     tw: "https://www.amazon.com/gp/redirect.html/ref=cm_sw_cl_tw_dp_15W5YPJXG8ZK0V2WN22S?_encoding=UTF8&location=https%3A%2F%2Ftwitter.com%2Fintent%2Ftweet%3Foriginal_referer%3Dhttps%25253A%25252F%25252Fwww.amazon.com%25252Fgp%25252Fproduct%25252FB08QV53WTG%25252Fref%25253Dcm_sw_r_tw_dp_15W5YPJXG8ZK0V2WN22S%25253Fie%25253DUTF8%252526psc%25253D1%26text%3DThe%2520Party%2520Don%27t%2520Start%2520Til%2520l%2520Croc%2520In%2520T-Shirt%2520The%2520Party%2520Don%27t%2520Start%2520Til%2520l%2520Croc%2520...%26related%3Damazondeals%252Camazonmp3%26url%3Dhttps%253A%252F%252Fwww.amazon.com%252Fdp%252FB08QV53WTG%252Fref%253Dcm_sw_r_tw_dp_15W5YPJXG8ZK0V2WN22S%253F_encoding%253DUTF8%2526psc%253D1%26via%3Damazon&token=E5FB53EC80CFED24755EDD959D627E22B8F921F5",
   };
 
+  const closeDisplay = () =>
+    setTimeout(() => {
+      setLinksDisplay({ display: "none" });
+    }, 500);
   return (
     <>
       <div className={`${style.linksDisplay}`} style={linksDisplay}>
-        <a href={href.email} target="_" title="Share via Email">
+        <a
+          href={href.email}
+          target="_"
+          title="Share via Email"
+          onClick={() => closeDisplay()}
+        >
           <AiOutlineMail className={style.pin} style={{ color: "black" }} />{" "}
           <span>Email</span>
         </a>
-        <a href={href.pin} target="_" title="Pin it on Pinterest">
+        <a
+          href={href.pin}
+          target="_"
+          title="Pin it on Pinterest"
+          onClick={() => closeDisplay()}
+        >
           <FaPinterest className={style.pin} style={{ color: "darkred" }} />
           <span>Pinterest</span>
         </a>
-        <a href={href.fb} target="_" title="Share on Facebook">
+        <a
+          href={href.fb}
+          target="_"
+          title="Share on Facebook"
+          onClick={() => closeDisplay()}
+        >
           <FaFacebook className={style.pin} style={{ color: "blue" }} />
           <span>Facebook</span>
         </a>
-        <a href={href.tw} target="_" title="Share on Twitter">
+        <a
+          href={href.tw}
+          target="_"
+          title="Share on Twitter"
+          onClick={() => closeDisplay()}
+        >
           <AiFillTwitterCircle
             className={style.pin}
             style={{ color: "skyblue" }}
