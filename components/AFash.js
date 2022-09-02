a1a37d6f77433a344b1c4637a3e08b9a97f11bb0
Ilsa <hamptonilsa96@gmail.com>
2022-09-02 16:12:00 -0400

    deleted extras

--- components/AFash.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function AFash() {
-  return <div>AFash</div>;
-}
--- components/Mid.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Mid() {
-  return <div>Mid</div>;
-}
