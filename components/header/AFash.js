16f7110e289f646cb120d57add2ea011389367b1
Ilsa <hamptonilsa96@gmail.com>
2022-09-06 14:21:52 -0400

    fixed lists to pull from arrays

--- components/header/AFash.js
+++ components/header/AFash.js
@@ -2,19 +2,24 @@ import React from "react";
 import style from "../../styles/Header.module.css";
 
 export default function AFash() {
+  const fashCat = [
+    "Women",
+    "Men",
+    "Kids",
+    "Luggage",
+    "Sales and Deals",
+    "New Arrivals",
+    "Our Brands",
+    "Primo try before you buy",
+  ];
   return (
     <div className={`${style.afash}`}>
       <div className="fashcats">
         <ul className={`${style.catsf}`}>
           <strong>AMAZON FASHION</strong>
-          <li className={`${style.lif}`}>Women</li>
-          <li className={`${style.lif}`}>Men</li>
-          <li className={`${style.lif}`}>Kids</li>
-          <li className={`${style.lif}`}>Luggage</li>
-          <li className={`${style.lif}`}>Sales and Deals</li>
-          <li className={`${style.lif}`}>New Arrivals</li>
-          <li className={`${style.lif}`}>Our Brands</li>
-          <li className={`${style.lif}`}>Primo try before you buy</li>
+          {fashCat.map((e) => {
+            return <li className={`${style.lif}`}>{e}</li>;
+          })}
         </ul>
       </div>
     </div>
--- components/header/Mid.js
+++ components/header/Mid.js
@@ -1,21 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import style from "../../styles/Header.module.css";
 import { BsList } from "react-icons/bs";
 
 export default function Mid() {
+  const categories = [
+    "Primo Video",
+    "Health and Household",
+    "Shopper Toolkit",
+    "Pet Supplies",
+    "Amazon Home",
+    "Pharmacy",
+    "Amazon Launchpad",
+  ];
+
   return (
     <div className={`${style.midhead}`}>
       <div className={`${style.allcat}`}>
         <BsList className={`${style.iconlist}`} />
       </div>
       <ul className={`${style.cats}`}>
-        <li className={`${style.li}`}>Primo Video</li>
-        <li className={`${style.li}`}>Health and Household</li>
-        <li className={`${style.li}`}>Shopper Toolkit</li>
-        <li className={`${style.li}`}>Pet Supplies</li>
-        <li className={`${style.li}`}>Amazon Home</li>
-        <li className={`${style.li}`}>Pharmacy</li>
-        <li className={`${style.li}`}>Amazon Launchpad</li>
+        {categories.map((e) => {
+          return <li className={`${style.li}`}>{e}</li>;
+        })}
       </ul>
     </div>
   );
