7f08062439062402c6b6c45f445417d2cedf5936
Jacob <jacob.d.walters22@gmail.com>
2022-09-05 21:44:46 -0400

    footer1 styleing

---
+++ components/Footer/Foot2.js
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "../../styles/Foot2.module.css";
+
+const Foot2 = () => {
+  return <div className={style.container2}>Foot2</div>;
+};
+
+export default Foot2;
