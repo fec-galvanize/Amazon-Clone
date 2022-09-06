fa86d2fcbcc45984755db9be967e6a4a985508a2
Ilsa <hamptonilsa96@gmail.com>
2022-09-06 09:24:05 -0400

    slight adjustments

--- components/header/AFash.js
+++ components/header/AFash.js
@@ -2,5 +2,20 @@ import react from "react";
 import style from "../../styles/Header.module.css";
 
 export default function AFash() {
-  return <div className={`${style.afash}`}>AFash</div>;
+  return (
+    <div className={`${style.afash}`}>
+      <div className="fashcats">
+        <ul className={`${style.catsf}`}>
+          AMAZON FASHION
+          <li className={`${style.lif}`}>Women</li>
+          <li className={`${style.lif}`}>Men</li>
+          <li className={`${style.lif}`}>Kids</li>
+          <li className={`${style.lif}`}>Luggage</li>
+          <li className={`${style.lif}`}>Sales and Deals</li>
+          <li className={`${style.lif}`}>New Arrivals</li>
+          <li className={`${style.lif}`}>Our Brands</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
