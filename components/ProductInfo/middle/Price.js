8d3ce95b497fd170b5ec48eada1d4566dcbf1e3b
Ilsa <hamptonilsa96@gmail.com>
2022-09-23 14:13:14 -0400

    price.js is access ready

--- components/ProductInfo/middle/Price.js
+++ components/ProductInfo/middle/Price.js
@@ -2,32 +2,39 @@ import style from "../../../styles/Middle.module.css";
 
 const Price = () => {
   return (
-    <div className={style.price}>
-      <div className={style.numbers}>
+    <div className={style.price} aria-label="price">
+      <div className={style.numbers} aria-label="numbers">
         <span className={style.symbol}>$</span>
         <span className={style.dollars}>17</span>
         <span className={style.cents}>99</span>
       </div>
-      <div className={style.primePlug}>
+      <div className={style.primePlug} aria-label="prime plug">
         <span>
           Get
           <b> Fast, Free Shipping </b>
           with
-          <a className={style.prime}> Amazon Prime </a>
+          <a className={style.prime} aria-label="get prime link">
+            {" "}
+            Amazon Prime{" "}
+          </a>
+          {/* this anchor tag doesn't have an href btw -IH*/}
         </span>
       </div>
-      <div className={style.freeReturns}>
+      <div className={style.freeReturns} aria-label="free returns">
         <span>&</span>
         <span className={style.free}> FREE Returns </span>
       </div>
-      <div className={style.merchPlug}>
-        <div className={style.image}>
+      <div className={style.merchPlug} aria-label="merch plug">
+        <div className={style.image} aria-label="hold picture of the merch">
           <img
             className={style.merchpic}
             alt="Amazon merch on demand"
             src="https://images-na.ssl-images-amazon.com/images/G/01/Merch/logo/amazon_merch_on_demand_logo2x._CB650496796_.png"
           ></img>
-          <span className={style.learn}> Learn more</span>
+          <span className={style.learn} aria-label="Learn">
+            {" "}
+            Learn more
+          </span>
         </div>
       </div>
     </div>
