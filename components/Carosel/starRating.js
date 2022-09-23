957963a84f71f99db57dd56c71408fd6558e2f37
Ilsa <hamptonilsa96@gmail.com>
2022-09-23 12:30:49 -0400

    fixed accessibility for starRating.js

--- components/Carosel/starRating.js
+++ components/Carosel/starRating.js
@@ -1,28 +1,26 @@
-import React, { useState } from 'react'
-import { Rating } from 'react-simple-star-rating'
+import React, { useState } from "react";
+import { Rating } from "react-simple-star-rating";
 import styles from "../../styles/caoursel2.module.css";
 
 const starPower = () => {
-  const [rating, setRating] = useState(0) // initial rating value
+  const [rating, setRating] = useState(0); // initial rating value
 
   // Catch Rating value
-  const handleRating = ({rate: number}) => {
-    setRating(rate)
-
-  }
+  const handleRating = ({ rate: number }) => {
+    setRating(rate);
+  };
 
   return (
-    <div className={styles.star}>
-      <Rating 
-      onClick={handleRating} 
-      ratingValue={rating} 
-      allowHover={false}
-      allowHalfIcon
-      size={20}
-      
-     />
+    <div className={styles.star} aria-label="star rating">
+      <Rating
+        onClick={handleRating}
+        ratingValue={rating}
+        allowHover={false}
+        allowHalfIcon
+        size={20}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default starPower;
