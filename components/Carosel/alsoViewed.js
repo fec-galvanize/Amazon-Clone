a49727095978dea2a432d8f2621ed00b59429761
Justin Melendez <104957885+CountDown2Extinction@users.noreply.github.com>
2022-09-23 15:30:40 -0500

    test

--- components/Carosel/alsoViewed.js
+++ components/Carosel/alsoViewed.js
@@ -13,8 +13,43 @@ const Alsoviewed = () => {
     speed: 500,
     slidesToShow: 7,
     slidesToScroll: 7,
+    initialSlide: 0,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+      ,
+      {
+        breakpoint: 220,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   };
 
   function SampleNextArrow(props) {
--- components/Carosel/related.js
+++ components/Carosel/related.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -13,10 +13,44 @@ const T_shirt_line = () => {
     speed: 500,
     slidesToShow: 7,
     slidesToScroll: 7,
+    initialSlide: 0,
     nextArrow: <SampleNextArrow />,
     prevArrow: <SamplePrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }
+      ,
+      {
+        breakpoint: 220,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
   };
-
   function SampleNextArrow(props) {
     const { className, style, onClick } = props;
     return (
